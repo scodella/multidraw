b52817ea2dd6dd64a6d91036e8282d042eea280b
Yutaro <yutaro.iiyama@cern.ch>
2018-10-22 11:51:28 +0200

    need to genDict for CMSSW <= 9

--- mkLinkDef.py
+++ mkLinkDef.py
@@ -14,18 +14,24 @@ sys.argv = []
 thisdir = os.path.dirname(os.path.realpath(__file__))
 
 if args.cmssw:
-    linkdef_path = 'src/LinkDef.h'
+    if int(os.environ['CMSSW_VERSION'].split('_')[1]) < 9:
+        # older SCRAM version cannot handle LinkDef properly -> we do it manually below
+        linkdef_path = 'interface/LinkDef.h'
+    else:
+        linkdef_path = 'src/LinkDef.h'
+
+    second_last_slash = thisdir.rfind('/', 0, thisdir.rfind('/') - 1)
+    thisdir_cmssw_rel = thisdir[second_last_slash + 1:]
 else:
     linkdef_path = 'obj/LinkDef.h'
 
-with open(linkdef_path, 'w') as out:
+with open(thisdir + '/' + linkdef_path, 'w') as out:
     for fname in os.listdir(thisdir + '/interface'):
-        if not fname.endswith('.h'):
+        if not fname.endswith('.h') or fname == 'LinkDef.h':
             continue
 
         if args.cmssw:
-            second_last_slash = thisdir.rfind('/', 0, thisdir.rfind('/') - 1)
-            out.write('#include "%s/interface/%s"\n' % (thisdir[second_last_slash + 1:], fname))
+            out.write('#include "%s/interface/%s"\n' % (thisdir_cmssw_rel, fname))
         else:
             out.write('#include "%s/interface/%s"\n' % (thisdir, fname))
 
@@ -40,7 +46,7 @@ with open(linkdef_path, 'w') as out:
     out.write('#pragma link C++ namespace multidraw;\n')
 
     for fname in os.listdir(thisdir + '/interface'):
-        if not fname.endswith('.h'):
+        if not fname.endswith('.h') or fname == 'LinkDef.h':
             continue
 
         if fname == 'TTreeFormulaCached.h':
@@ -58,3 +64,30 @@ if args.cmssw:
         out.write('<export>\n')
         out.write('  <lib name="1"/>\n')
         out.write('</export>\n')
+
+    if int(os.environ['CMSSW_VERSION'].split('_')[1]) < 10:
+        import subprocess
+        import shutil
+        import glob
+
+        dict_name = 'dictMultiDraw'
+        pcm_path = os.environ['CMSSW_BASE'] + '/lib/' + os.environ['SCRAM_ARCH'] + '/' + dict_name + '_rdict.pcm'
+
+        try:
+            os.unlink(pcm_path)
+        except:
+            pass
+        
+        cmd = ['rootcling', '-f', thisdir + '/src/' + dict_name + '.cc']
+        cmd.append('-I' + os.environ['CMSSW_BASE'] + '/src')
+        proc = subprocess.Popen(['root-config', '--incdir'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        root_inc = proc.communicate()[0].strip()
+        cmd.append('-I' + root_inc)
+        cmd.append(thisdir + '/interface/LinkDef.h')
+        proc = subprocess.Popen(cmd)
+        proc.communicate()
+        if proc.returncode != 0:
+            raise RuntimeError('rootcling failed')
+
+        shutil.copyfile(thisdir + '/src/' + dict_name + '_rdict.pcm', pcm_path)
+        os.unlink(thisdir + '/src/' + dict_name + '_rdict.pcm')
