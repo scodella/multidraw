1e9a6284fca0753800441b7742c607670f9ef62b
Yutaro <yutaro.iiyama@cern.ch>
2019-01-27 08:57:17 +0100

    adding comments on why we new every single formula

--- interface/FormulaLibrary.h
+++ interface/FormulaLibrary.h
@@ -19,11 +19,17 @@ namespace multidraw {
     ~FormulaLibrary() {}
 
     //! Create a new formula object with a shared cache.
+    /*!
+     * ExprFiller uses a TTreeFormulaManager to aggregate formulas. Adding a formula to a manager overwrites
+     * an attribute of the formula object itself. Since the grouping can be different for each filler, this
+     * means we cannot cache the formulas themselves and reuse among the fillers.
+     */
     TTreeFormulaCachedPtr getFormula(char const* expr, bool silent = false);
 
     void updateFormulaLeaves();
     void resetCache();
 
+    unsigned size() const { return formulas_.size(); }
     void prune();
 
   private:
